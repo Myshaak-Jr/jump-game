0b4675819b40abb00e965c182bb54e8a40ad2578
Matěj Smetana <matej@smetana.online>
2024-09-04 20:41:07 +0200

    changed how fuel works

--- src/scenes/level/player.py
+++ src/scenes/level/player.py
@@ -26,17 +26,26 @@ class Player(IHasPos):
 		self._won = False
 
 		# Fuel
-		self._MAX_FUEL = 30
-		self._fuel = self._MAX_FUEL
+		self._max_fuel = 10
+		self._fuel = self._max_fuel
+		self._fuel_regen_speed = 20
+
+		# Wheels
+		self._wheel_speed = 50
+		self._wheel_friction = 1.5
 
 		# Load the player sprites
 		self._load_sprites()
-		
+
+		# On ground
+		self._on_ground: dict[pymunk.Shape, bool] = {}
+
 		# Pymunk representation
 		self._load_body(space, x, y)
 
 		handler = space.add_collision_handler(CollisionType.PLAYER.value, CollisionType.GROUND.value)
-		handler.post_solve = self._hit_ground
+		handler.begin = self._hit_ground
+		handler.separate = self._separate_ground
 
 		# Apply a force to the right
 		self._push_right()
@@ -47,89 +56,99 @@ class Player(IHasPos):
 		self._wheel_cache: pygame.Surface | None = None
 
 	def _hit_ground(self, arbiter: pymunk.Arbiter, space: pymunk.Space, data: dict[str, Any]) -> bool:
-		if arbiter.normal.y > 0.8:
-			self._fuel = self._MAX_FUEL
+		if arbiter.normal.y > 0.3:
+			self._on_ground[arbiter.shapes[0]] = True
+		return True
+	
+	def _separate_ground(self, arbiter: pymunk.Arbiter, space: pymunk.Space, data: dict[str, Any]) -> bool:
+		self._on_ground[arbiter.shapes[0]] = False
 		return True
 
+	def is_on_ground(self) -> bool:
+		return any(self._on_ground.values())
+
 	def _load_body(self, space: pymunk.Space, x: float, y: float):
-		self._SCALE = 1 / 100 * 6
+		self._scale = 1 / 100 * 6
 
-		self._CHASSI_WIDTH = 80 * self._SCALE
-		self._CHASSI_HEIGHT = 60 * self._SCALE
+		self._chassi_width = 80 * self._scale
+		self._chassi_height = 60 * self._scale
 
-		self._AXLE_LENGTH = 30 * self._SCALE
-		self._AXLE_RADIUS = 3 * self._SCALE
-		self._AXLE_ANGLE = 40
+		self._axle_lenght = 30 * self._scale
+		self._axle_radius = 3 * self._scale
+		self._axle_angle = 40
 
-		self._WHEEL_RADIUS = 20 * self._SCALE
+		self._wheel_radius = 20 * self._scale
 
-		self._AXLE1_TANGENT = pymunk.Vec2d(1, 0).rotated(math.radians(-self._AXLE_ANGLE))
-		self._AXLE1_P1, self._AXLE1_P2 = -self._AXLE_LENGTH / 2 * self._AXLE1_TANGENT, self._AXLE_LENGTH / 2 * self._AXLE1_TANGENT
+		self._left_axle_tangent = pymunk.Vec2d(1, 0).rotated(math.radians(-self._axle_angle))
+		self._left_axle_p1, self._left_axle_p2 = -self._axle_lenght / 2 * self._left_axle_tangent, self._axle_lenght / 2 * self._left_axle_tangent
 
-		self._AXLE2_TANGENT = pymunk.Vec2d(1, 0).rotated(math.radians(self._AXLE_ANGLE))
-		self._AXLE2_P1, self._AXLE2_P2 = -self._AXLE_LENGTH / 2 * self._AXLE2_TANGENT, self._AXLE_LENGTH / 2 * self._AXLE2_TANGENT
+		self._right_axle_tangent = pymunk.Vec2d(1, 0).rotated(math.radians(self._axle_angle))
+		self._right_axle_p1, self._right_axle_p2 = -self._axle_lenght / 2 * self._right_axle_tangent, self._axle_lenght / 2 * self._right_axle_tangent
+
+		SPRING_STIFFNESS = 2000
+		SPRING_DAMPING = 20
 		
 		mass = 20
 		moment = pymunk.moment_for_box(mass, (
-			self._CHASSI_WIDTH + abs(self._AXLE1_TANGENT.x) * self._AXLE_LENGTH + abs(self._AXLE2_TANGENT.x) * self._AXLE_LENGTH,
-			self._CHASSI_HEIGHT
+			self._chassi_width + abs(self._left_axle_tangent.x) * self._axle_lenght + abs(self._right_axle_tangent.x) * self._axle_lenght,
+			self._chassi_height
 		))
 		self._chassi_b = pymunk.Body(mass, moment)
-		self._chassi_s = pymunk.Poly.create_box(self._chassi_b, (self._CHASSI_WIDTH, self._CHASSI_HEIGHT))
+		self._chassi_s = pymunk.Poly.create_box(self._chassi_b, (self._chassi_width, self._chassi_height))
 		self._chassi_s.friction = 0.7
-		self._chassi_b.center_of_gravity = (0, self._CHASSI_HEIGHT * 0.6)
+		self._chassi_b.center_of_gravity = (0, self._chassi_height * 0.6)
 		self._chassi_s.collision_type = CollisionType.PLAYER.value
 		space.add(self._chassi_b, self._chassi_s)
 
 		mass = 2
-		moment = pymunk.moment_for_circle(mass, 0, self._WHEEL_RADIUS)
-		self._wheel1_b = pymunk.Body(mass, 2)
-		self._wheel1_s = pymunk.Circle(self._wheel1_b, self._WHEEL_RADIUS)
-		self._wheel1_s.friction = 1.5
-		self._wheel1_s.collision_type = CollisionType.PLAYER.value
-		space.add(self._wheel1_b, self._wheel1_s)
+		moment = pymunk.moment_for_circle(mass, 0, self._wheel_radius)
+		self.left_wheel_b = pymunk.Body(mass, 2)
+		self.left_wheel_s = pymunk.Circle(self.left_wheel_b, self._wheel_radius)
+		self.left_wheel_s.friction = self._wheel_friction
+		self.left_wheel_s.collision_type = CollisionType.PLAYER.value
+		space.add(self.left_wheel_b, self.left_wheel_s)
 
-		self._wheel2_b = pymunk.Body(mass, moment)
-		self._wheel2_s = pymunk.Circle(self._wheel2_b, self._WHEEL_RADIUS)
-		self._wheel2_s.friction = 1.5
-		self._wheel2_s.collision_type = CollisionType.PLAYER.value
-		space.add(self._wheel2_b, self._wheel2_s)
+		self._right_wheel_b = pymunk.Body(mass, moment)
+		self._right_wheel_s = pymunk.Circle(self._right_wheel_b, self._wheel_radius)
+		self._right_wheel_s.friction = self._wheel_friction
+		self._right_wheel_s.collision_type = CollisionType.PLAYER.value
+		space.add(self._right_wheel_b, self._right_wheel_s)
 
 		mass = 0.1
-		moment = pymunk.moment_for_segment(mass, self._AXLE1_P1, self._AXLE1_P2, self._AXLE_RADIUS)
-		self._axle1_b = pymunk.Body(mass, moment)
-		self._axle1_s = pymunk.Segment(self._axle1_b, self._AXLE1_P1, self._AXLE1_P2, self._AXLE_RADIUS)
-		self._axle1_s.friction = 1.5
-		space.add(self._axle1_b, self._axle1_s)
+		moment = pymunk.moment_for_segment(mass, self._left_axle_p1, self._left_axle_p2, self._axle_radius)
+		self._left_axle_b = pymunk.Body(mass, moment)
+		self._left_axle_s = pymunk.Segment(self._left_axle_b, self._left_axle_p1, self._left_axle_p2, self._axle_radius)
+		self._left_axle_s.friction = 1.5
+		space.add(self._left_axle_b, self._left_axle_s)
 
-		moment = pymunk.moment_for_segment(mass, self._AXLE2_P1, self._AXLE2_P2, self._AXLE_RADIUS)
-		self._axle2_b = pymunk.Body(mass, moment)
-		self._axle2_s = pymunk.Segment(self._axle2_b, self._AXLE2_P1, self._AXLE2_P2, self._AXLE_RADIUS)
-		self._axle2_s.friction = 1.5
-		space.add(self._axle2_b, self._axle2_s)
+		moment = pymunk.moment_for_segment(mass, self._right_axle_p1, self._right_axle_p2, self._axle_radius)
+		self._right_axle_b = pymunk.Body(mass, moment)
+		self._right_axle_s = pymunk.Segment(self._right_axle_b, self._right_axle_p1, self._right_axle_p2, self._axle_radius)
+		self._right_axle_s.friction = 1.5
+		space.add(self._right_axle_b, self._right_axle_s)
 
 		self.set_pos(Vec2(x, y))
 
-		j1 = pymunk.PivotJoint(self._chassi_b, self._axle1_b,
-			(-(self._CHASSI_WIDTH / 2 - self._AXLE_RADIUS), self._CHASSI_HEIGHT / 2 - self._AXLE_RADIUS),
-			self._AXLE1_P2
+		j1 = pymunk.PivotJoint(self._chassi_b, self._left_axle_b,
+			(-(self._chassi_width / 2 - self._axle_radius), self._chassi_height / 2 - self._axle_radius),
+			self._left_axle_p2
 		)
 		j1.collide_bodies = False
-		s1 = pymunk.DampedRotarySpring(self._chassi_b, self._axle1_b, 0, 2000, 9)
+		s1 = pymunk.DampedRotarySpring(self._chassi_b, self._left_axle_b, 0, SPRING_STIFFNESS, SPRING_DAMPING)
 		space.add(j1, s1)
 
-		j2 = pymunk.PivotJoint(self._chassi_b, self._axle2_b,
-			(self._CHASSI_WIDTH / 2 - self._AXLE_RADIUS, self._CHASSI_HEIGHT / 2 - self._AXLE_RADIUS),
-			self._AXLE2_P1
+		j2 = pymunk.PivotJoint(self._chassi_b, self._right_axle_b,
+			(self._chassi_width / 2 - self._axle_radius, self._chassi_height / 2 - self._axle_radius),
+			self._right_axle_p1
 		)
 		j2.collide_bodies = False
-		s2 = pymunk.DampedRotarySpring(self._chassi_b, self._axle2_b, 0, 2000, 9)
+		s2 = pymunk.DampedRotarySpring(self._chassi_b, self._right_axle_b, 0, SPRING_STIFFNESS, SPRING_DAMPING)
 		space.add(j2, s2)
 
-		j3 = pymunk.PivotJoint(self._axle1_b, self._wheel1_b, self._AXLE1_P1, (0, 0))
+		j3 = pymunk.PivotJoint(self._left_axle_b, self.left_wheel_b, self._left_axle_p1, (0, 0))
 		j3.collide_bodies = False
 		space.add(j3)
-		j4 = pymunk.PivotJoint(self._axle2_b, self._wheel2_b, self._AXLE2_P2, (0, 0))
+		j4 = pymunk.PivotJoint(self._right_axle_b, self._right_wheel_b, self._right_axle_p2, (0, 0))
 		j4.collide_bodies = False
 		space.add(j4)
 	
@@ -145,39 +164,39 @@ class Player(IHasPos):
 
 	def render(self, screen: pygame.Surface, camera: Camera):
 		# Draw the axles
-		axle1_p1 = camera.apply_pos(Vec2(self._axle1_b.local_to_world(self._AXLE1_P1)))
-		axle1_p2 = camera.apply_pos(
+		left_axle_p1 = camera.apply_pos(Vec2(self._left_axle_b.local_to_world(self._left_axle_p1)))
+		left_axle_p2 = camera.apply_pos(
 			Vec2(
 				self._chassi_b.local_to_world(
 					(
-						-self._CHASSI_WIDTH / 2 + self._AXLE_RADIUS,
-						self._CHASSI_HEIGHT / 2 - self._AXLE_RADIUS
+						-self._chassi_width / 2 + self._axle_radius,
+						self._chassi_height / 2 - self._axle_radius
 					)
 				)
 			)
 		)
-		axle2_p1 = camera.apply_pos(
+		right_axle_p1 = camera.apply_pos(
 			Vec2(
 				self._chassi_b.local_to_world(
 					(
-						self._CHASSI_WIDTH / 2 - self._AXLE_RADIUS,
-						self._CHASSI_HEIGHT / 2 - self._AXLE_RADIUS
+						self._chassi_width / 2 - self._axle_radius,
+						self._chassi_height / 2 - self._axle_radius
 					)
 				)
 			)
 		)
-		axle2_p2 = camera.apply_pos(Vec2(self._axle2_b.local_to_world(self._AXLE2_P2)))
-		axle_width = camera.get_zoom() * self._AXLE_RADIUS * 2
+		right_axle_p2 = camera.apply_pos(Vec2(self._right_axle_b.local_to_world(self._right_axle_p2)))
+		axle_width = camera.get_zoom() * self._axle_radius * 2
 
-		pygame.draw.line(screen, "#D4D7D9", axle1_p1.to_tuple(), axle1_p2.to_tuple(), int(axle_width))
-		pygame.draw.line(screen, "#D4D7D9", axle2_p1.to_tuple(), axle2_p2.to_tuple(), int(axle_width))
+		pygame.draw.line(screen, "#D4D7D9", left_axle_p1.to_tuple(), left_axle_p2.to_tuple(), int(axle_width))
+		pygame.draw.line(screen, "#D4D7D9", right_axle_p1.to_tuple(), right_axle_p2.to_tuple(), int(axle_width))
 
 		# Draw the chassi
 
 		if self._last_zoom != camera.get_zoom() or self._car_cache is None:
 			self._last_zoom = camera.get_zoom()
 			aspect_ratio = self._car.get_width() / self._car.get_height()
-			new_height = int(camera.get_zoom() * self._CHASSI_HEIGHT)
+			new_height = int(camera.get_zoom() * self._chassi_height)
 			new_width = int(new_height * aspect_ratio)
 			self._car_cache = pygame.transform.scale(self._car, (new_width, new_height))
 		
@@ -201,38 +220,29 @@ class Player(IHasPos):
 		# pygame.draw.circle(screen, (0, 0, 255), cog.to_tuple(), 3)
 
 		# Draw the wheels
-		wheel_radius = int(camera.get_zoom() * (self._WHEEL_RADIUS * 1.1)) # 1.1 because the wheel image is a bit smaller than the actual wheel
+		wheel_radius = int(camera.get_zoom() * (self._wheel_radius * 1.1)) # 1.1 because the wheel image is a bit smaller than the actual wheel
 
 		if self._last_zoom != camera.get_zoom() or self._wheel_cache is None:
 			self._last_zoom = camera.get_zoom()
 			self._wheel_cache = pygame.transform.scale(self._wheel, (wheel_radius * 2, wheel_radius * 2))
+			
+		left_wheel_screen_pos = camera.apply_pos(Vec2(self.left_wheel_b.position))
+		left_wheel_angle = self.left_wheel_b.angle
+		left_wheel_sprite = pygame.transform.rotate(self._wheel_cache, -math.degrees(left_wheel_angle))
+		left_wheel_x, left_wheel_y = left_wheel_sprite.get_size()
+		left_wheel_x = left_wheel_screen_pos.x - left_wheel_x / 2
+		left_wheel_y = left_wheel_screen_pos.y - left_wheel_y / 2
+
+		screen.blit(left_wheel_sprite, (left_wheel_x, left_wheel_y))
+
+		right_wheel_screen_pos = camera.apply_pos(Vec2(self._right_wheel_b.position))
+		right_wheel_angle = self._right_wheel_b.angle
+		right_wheel_sprite = pygame.transform.rotate(self._wheel_cache, -math.degrees(right_wheel_angle))
+		right_wheel_x, right_wheel_y = right_wheel_sprite.get_size()
+		right_wheel_x = right_wheel_screen_pos.x - right_wheel_x / 2
+		right_wheel_y = right_wheel_screen_pos.y - right_wheel_y / 2
 		
-		wheel1_screen_pos = camera.apply_pos(Vec2(self._wheel1_b.position))
-		wheel1_angle = self._wheel1_b.angle
-		wheel1_sprite = pygame.transform.rotate(self._wheel_cache, -math.degrees(wheel1_angle))
-		wheel1_x, wheel1_y = wheel1_sprite.get_size()
-		wheel1_x = wheel1_screen_pos.x - wheel1_x / 2
-		wheel1_y = wheel1_screen_pos.y - wheel1_y / 2
-
-		screen.blit(wheel1_sprite, (wheel1_x, wheel1_y))
-
-		wheel2_screen_pos = camera.apply_pos(Vec2(self._wheel2_b.position))
-		wheel2_angle = self._wheel2_b.angle
-		wheel2_sprite = pygame.transform.rotate(self._wheel_cache, -math.degrees(wheel2_angle))
-		wheel2_x, wheel2_y = wheel2_sprite.get_size()
-		wheel2_x = wheel2_screen_pos.x - wheel2_x / 2
-		wheel2_y = wheel2_screen_pos.y - wheel2_y / 2
-		
-		screen.blit(wheel2_sprite, (wheel2_x, wheel2_y))
-
-		# left_point, right_point, force_left, force_right = self._get_force_points()
-		# left_point_screen = camera.apply_pos(Vec2(left_point))
-		# right_point_screen = camera.apply_pos(Vec2(right_point))
-		# force_left_screen = camera.apply_pos(Vec2(left_point + force_left/1000))
-		# force_right_screen = camera.apply_pos(Vec2(right_point + force_right/1000))
-
-		# pygame.draw.line(screen, (255, 0, 0), left_point_screen.to_tuple(), force_left_screen.to_tuple(), 2)
-		# pygame.draw.line(screen, (255, 0, 0), right_point_screen.to_tuple(), force_right_screen.to_tuple(), 2)
+		screen.blit(right_wheel_sprite, (right_wheel_x, right_wheel_y))
 
 	def render_fuel_bar(self, screen: pygame.Surface):
 		x = 10
@@ -242,39 +252,37 @@ class Player(IHasPos):
 
 		pygame.draw.rect(screen, (0, 0, 0), (x-3, y-3, width+6, height+6))
 
-		fuel_width = width * self._fuel / self._MAX_FUEL
+		fuel_width = width * self._fuel / self._max_fuel
 		pygame.draw.rect(screen, (0, 255, 0), (x, y, fuel_width, height))
 
 	def set_pos(self, pos: Vec2):
 		new_pos = pymunk.Vec2d(pos.x, pos.y)
 		self._chassi_b.position = new_pos
-		self._axle1_b.position = new_pos + (
-			-(self._CHASSI_WIDTH / 2 - self._AXLE_RADIUS) - self._AXLE1_P2.x,
-			self._CHASSI_HEIGHT / 2 - self._AXLE1_P2.y - self._AXLE_RADIUS
+		self._left_axle_b.position = new_pos + (
+			-(self._chassi_width / 2 - self._axle_radius) - self._left_axle_p2.x,
+			self._chassi_height / 2 - self._left_axle_p2.y - self._axle_radius
 		)
-		self._axle2_b.position = new_pos + (
-			self._CHASSI_WIDTH / 2 - self._AXLE2_P1.x - self._AXLE_RADIUS,
-			self._CHASSI_HEIGHT / 2 - self._AXLE2_P1.y - self._AXLE_RADIUS
+		self._right_axle_b.position = new_pos + (
+			self._chassi_width / 2 - self._right_axle_p1.x - self._axle_radius,
+			self._chassi_height / 2 - self._right_axle_p1.y - self._axle_radius
 		)
 
-		self._wheel1_b.position = self._axle1_b.position + self._AXLE1_P1
-		self._wheel2_b.position = self._axle2_b.position + self._AXLE2_P2
+		self.left_wheel_b.position = self._left_axle_b.position + self._left_axle_p1
+		self._right_wheel_b.position = self._right_axle_b.position + self._right_axle_p2
 
 	def get_mass(self) -> float:
-		return self._chassi_b.mass + self._wheel1_b.mass + self._wheel2_b.mass + self._axle1_b.mass + self._axle2_b.mass
+		return self._chassi_b.mass + self.left_wheel_b.mass + self._right_wheel_b.mass + self._left_axle_b.mass + self._right_axle_b.mass
 
-	def _get_force_points(self) -> tuple[pymunk.Vec2d, pymunk.Vec2d, pymunk.Vec2d, pymunk.Vec2d]:
+	def _get_force_points(self, force_strength: float) -> tuple[pymunk.Vec2d, pymunk.Vec2d, pymunk.Vec2d, pymunk.Vec2d]:
 		a = 0.1
 		c = 0.7
 		d = 0.2
 
-		x_offset = self._CHASSI_WIDTH / 2
+		x_offset = self._chassi_width / 2
 		y_offset = 0
 
 		left_point = pymunk.Vec2d(-x_offset, y_offset).rotated(self._chassi_b.angle) + self._chassi_b.position
 		right_point = pymunk.Vec2d(x_offset, y_offset).rotated(self._chassi_b.angle) + self._chassi_b.position
-
-		force_strength = self._calc_thrust_up_force() / 2
 
 		angle_mult_left = (1 - math.sin(self._chassi_b.angle)) / 2
 		speed_mult_left = c*exponensial_logarithmic_function(-a*self._chassi_b.angular_velocity) + d
@@ -288,38 +296,39 @@ class Player(IHasPos):
 
 	def physics_update(self, dt: float):
 		if not self._alive: return
-		if self._chassi_b.velocity.x < self._planet.game_speed:
-			self._push_right()
 
+		# Modify the fuel
+		if self.is_on_ground():
+			self._fuel += self._fuel_regen_speed * dt
+			self._fuel = min(self._fuel, self._max_fuel)
+
+		# Handle the input
 		key_state = pygame.key.get_pressed()
 		if key_state[pygame.K_SPACE] and self._fuel > 0:
-			left_point, right_point, force_left, force_right = self._get_force_points()
+			force = self._calc_thrust_up_force()
+			left_point, right_point, force_left, force_right = self._get_force_points(force)
 			self._chassi_b.apply_force_at_world_point(force_left, left_point)
 			self._chassi_b.apply_force_at_world_point(force_right, right_point)
 
-			if self._chassi_b.velocity.y < 0.0:
-				self._fuel -= math.exp(-self._chassi_b.velocity.y * self._player_data.thrust_decay) * dt
-			else:
-				self._fuel -= dt
-			self._fuel = max(0, self._fuel)
+			self._fuel += force / (self._player_data.thrust_up * self.get_mass())
+			self._fuel = max(self._fuel, 0)
 
 		elif key_state[pygame.K_LSHIFT] and self._fuel > 0:
-			point = pymunk.Vec2d(0, self._CHASSI_HEIGHT).rotated(self._chassi_b.angle) + self._chassi_b.position
+			force = self._player_data.thrust_down * self.get_mass()
+			point = pymunk.Vec2d(0, self._chassi_height).rotated(self._chassi_b.angle) + self._chassi_b.position
+			self._chassi_b.apply_force_at_world_point(pymunk.Vec2d(0, force), point)
 
-			thrust = self._player_data.thrust_down * self.get_mass()
-			self._chassi_b.apply_force_at_world_point(pymunk.Vec2d(0, thrust), point)
-
-			self._fuel -= dt
-			self._fuel = max(0, self._fuel)
+			self._fuel -= force / (self._player_data.thrust_up * self.get_mass())
+			self._fuel = max(self._fuel, 0)
 		
 		if key_state[pygame.K_a]:
-			self._wheel1_b.angular_velocity = -30
-			self._wheel2_b.angular_velocity = -30
+			self.left_wheel_b.angular_velocity = -self._wheel_speed
+			self._right_wheel_b.angular_velocity = -self._wheel_speed
 
 		elif key_state[pygame.K_d]:
-			self._wheel1_b.angular_velocity = 30
-			self._wheel2_b.angular_velocity = 30
-
+			self.left_wheel_b.angular_velocity = self._wheel_speed
+			self._right_wheel_b.angular_velocity = self._wheel_speed
+		
 	def update(self, level_size: Vec2, camera: Camera):
 		# cap the angular velocity
 		self._chassi_b.angular_velocity = max(min(self._chassi_b.angular_velocity, 10), -10)
